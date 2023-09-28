c8af9f51d8782178df69ca897b7049c13bd6d405
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-09-28 14:56:00 +0200

    + config with stages

--- run_tests.sh
+++ run_tests.sh
@@ -11,16 +11,25 @@ else
   echo "Build: SUCCESS"
 fi
 
-cd ./target/debug
+mv ./target/debug/* ./
 
 result_exit_code=0
 
 # TEST CREATE ORGANIZATION SUCCESSFULLY, EXPECTED EXIT CODE: 0
 
+DEFAULT_AMQP_CONNECTION_FILE="./config/local/amqp_connection.json"
+TEST_AMQP_CONNECTION_FILE=${TEST_AMQP_CONNECTION_FILE:=$DEFAULT_AMQP_CONNECTION_FILE}
+
+DEFAULT_MONGODB_CONNECTION_FILE="./config/local/mongodb_connection.json"
+TEST_MONGODB_CONNECTION_FILE=${TEST_MONGODB_CONNECTION_FILE:=$DEFAULT_MONGODB_CONNECTION_FILE}
+
+DEFAULT_AMQP_API_FILE="./config/local/amqp_api.json"
+TEST_AMQP_API_FILE=${TEST_AMQP_API_FILE:=$DEFAULT_AMQP_API_FILE}
+
 DEFAULT_AMQP_CONNECTION_URI="amqp://guest:guest@127.0.0.1:5672"
 TEST_AMQP_CONNECTION_URI=${TEST_AMQP_CONNECTION_URI:=$DEFAULT_AMQP_CONNECTION_URI}
 
-./cp-organization $TEST_AMQP_CONNECTION_URI &
+./cp-organization $TEST_AMQP_CONNECTION_FILE $TEST_MONGODB_CONNECTION_FILE $TEST_AMQP_API_FILE &
 impl_pid=$!
 
 sleep 1
@@ -35,4 +44,14 @@ else
 fi
 
 kill $impl_pid
+
+rm ./cp-organization
+rm ./cp-organization.d
+rm ./test_create_organization_successfully
+rm ./test_create_organization_successfully.d
+rm -R ./build
+rm -R ./deps
+rm -R ./examples
+rm -R ./incremental
+
 exit $result_exit_code
