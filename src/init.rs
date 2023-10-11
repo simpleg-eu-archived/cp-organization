aa6f0d11a0e5488db20cc772bee6b9edff4dc4c0
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-11 20:08:32 +0200

    + minimize implementation code within main

---
+++ src/init.rs
@@ -0,0 +1,204 @@
+use std::{
+    io::{Error, ErrorKind},
+    sync::Arc,
+};
+
+use cp_microservice::{
+    core::secrets::secrets_manager::SecretsManager,
+    r#impl::{
+        api::{
+            server::input::token_manager::open_id_connect_config::OpenIdConnectConfig,
+            shared::amqp_api_entry::AmqpApiEntry,
+        },
+        core::bitwarden_secrets_manager::BitwardenSecretsManager,
+    },
+};
+use mongodb::{options::ClientOptions, Client};
+use multiple_connections_lapin_wrapper::config::amqp_connect_config::AmqpConnectConfig;
+
+const AMQP_CONNECTION_CONFIG_SECRET: &str = "CP_ORGANIZATION_AMQP_CONNECTION_SECRET";
+const MONGODB_CONNECTION_CONFIG_SECRET: &str = "CP_ORGANIZATION_MONGODB_CONNECTION_SECRET";
+
+pub fn get_secrets_manager() -> Result<Arc<dyn SecretsManager>, Error> {
+    let access_token = match std::env::args().nth(1) {
+        Some(access_token) => access_token,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "no access token provided",
+            ));
+        }
+    };
+
+    Ok(Arc::new(BitwardenSecretsManager::new(access_token)))
+}
+
+pub fn get_amqp_connection_config(
+    secrets_manager: &Arc<dyn SecretsManager>,
+) -> Result<AmqpConnectConfig, Error> {
+    let secret_id = match std::env::var(AMQP_CONNECTION_CONFIG_SECRET) {
+        Ok(secret_id) => secret_id,
+        Err(_) => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!(
+                    "failed to read secret id '{}'",
+                    AMQP_CONNECTION_CONFIG_SECRET
+                ),
+            ));
+        }
+    };
+
+    let amqp_connection_config_json = match secrets_manager.get(&secret_id) {
+        Some(amqp_connection_config_json) => amqp_connection_config_json,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("no secret with id '{}'", &secret_id),
+            ));
+        }
+    };
+
+    let amqp_connection_config =
+        match serde_json::from_str::<AmqpConnectConfig>(&amqp_connection_config_json) {
+            Ok(amqp_connection_config) => amqp_connection_config,
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("secret contains invalid amqp connection config: {}", &error),
+                ));
+            }
+        };
+
+    Ok(amqp_connection_config)
+}
+
+pub fn get_amqp_api() -> Result<Vec<AmqpApiEntry>, Error> {
+    let amqp_api_file = match std::env::args().nth(2) {
+        Some(amqp_api_file) => amqp_api_file,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "no amqp api file provided",
+            ));
+        }
+    };
+
+    let amqp_api_file_content = match std::fs::read_to_string(&amqp_api_file) {
+        Ok(content) => content,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                format!(
+                    "failed to find amqp api file '{}': {}",
+                    &amqp_api_file, &error
+                ),
+            ))
+        }
+    };
+
+    let amqp_api = match serde_json::from_str::<Vec<AmqpApiEntry>>(&amqp_api_file_content) {
+        Ok(amqp_api) => amqp_api,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                format!("failed to deserialize AMQP API file: {}", &error),
+            ))
+        }
+    };
+
+    Ok(amqp_api)
+}
+
+pub fn get_openid_connect_config() -> Result<OpenIdConnectConfig, Error> {
+    let openid_connect_config_file = match std::env::args().nth(3) {
+        Some(openid_connect_config_file) => openid_connect_config_file,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "no openid connect config file provided",
+            ));
+        }
+    };
+
+    let openid_connect_config_file_content =
+        match std::fs::read_to_string(&openid_connect_config_file) {
+            Ok(content) => content,
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::NotFound,
+                    format!(
+                        "failed to find openid connect config file '{}': {}",
+                        &openid_connect_config_file, &error
+                    ),
+                ))
+            }
+        };
+
+    let openid_connect_config =
+        match serde_json::from_str::<OpenIdConnectConfig>(&openid_connect_config_file_content) {
+            Ok(openid_connect_config) => openid_connect_config,
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!(
+                        "failed to deserialize OpenID Connect config file: {}",
+                        &error
+                    ),
+                ))
+            }
+        };
+
+    Ok(openid_connect_config)
+}
+
+pub fn get_mongodb_client(secrets_manager: &Arc<dyn SecretsManager>) -> Result<Client, Error> {
+    let secret_id = match std::env::var(MONGODB_CONNECTION_CONFIG_SECRET) {
+        Ok(secret_id) => secret_id,
+        Err(_) => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!(
+                    "failed to read secret id '{}'",
+                    MONGODB_CONNECTION_CONFIG_SECRET
+                ),
+            ));
+        }
+    };
+
+    let mongodb_connection_config_json = match secrets_manager.get(&secret_id) {
+        Some(mongodb_connection_config_json) => mongodb_connection_config_json,
+        None => {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("no secret with id '{}'", &secret_id),
+            ));
+        }
+    };
+
+    let mongodb_client_options =
+        match serde_json::from_str::<ClientOptions>(&mongodb_connection_config_json) {
+            Ok(mongodb_client_options) => mongodb_client_options,
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!(
+                        "failed to deserialize MongoDB connection config: {}",
+                        &error
+                    ),
+                ))
+            }
+        };
+
+    let mongodb_client = match Client::with_options(mongodb_client_options) {
+        Ok(mongodb_client) => mongodb_client,
+        Err(error) => {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                format!("failed to create MongoDB client: {}", &error),
+            ))
+        }
+    };
+
+    Ok(mongodb_client)
+}
--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,19 @@ use multiple_connections_lapin_wrapper::config::amqp_connect_config::AmqpConnect
 
 use crate::{
     api::{api_actions::get_api_actions, api_plugins::get_api_plugins},
+    init::{
+        get_amqp_api, get_amqp_connection_config, get_mongodb_client, get_openid_connect_config,
+        get_secrets_manager,
+    },
     logic::{logic_executors::get_logic_executors, logic_request::LogicRequest},
     storage::storage_request::StorageRequest,
 };
 
 pub mod api;
 pub mod error;
+pub mod init;
 pub mod logic;
 pub mod storage;
-
-const AMQP_CONNECTION_CONFIG_SECRET: &str = "CP_ORGANIZATION_AMQP_CONNECTION_SECRET";
-const MONGODB_CONNECTION_CONFIG_SECRET: &str = "CP_ORGANIZATION_MONGODB_CONNECTION_SECRET";
 
 #[tokio::main]
 pub async fn main() -> Result<(), Error> {
@@ -72,14 +74,14 @@ pub async fn main() -> Result<(), Error> {
         storage_request_sender,
     };
 
-    let storage_connection = get_mongodb_client(&secrets_manager)?;
-
     match try_initialize_microservice(api_initialization_package, logic_initialization_package)
         .await
     {
         Ok(_) => (),
         Err(error) => return Err(error),
     };
+
+    let storage_connection = get_mongodb_client(&secrets_manager)?;
 
     let storage_dispatch = crate::storage::dispatch::Dispatch::new(
         storage_request_receiver.clone(),
@@ -89,188 +91,4 @@ pub async fn main() -> Result<(), Error> {
     storage_dispatch.run().await;
 
     Ok(())
-}
-
-fn get_secrets_manager() -> Result<Arc<dyn SecretsManager>, Error> {
-    let access_token = match std::env::args().nth(1) {
-        Some(access_token) => access_token,
-        None => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                "no access token provided",
-            ));
-        }
-    };
-
-    Ok(Arc::new(BitwardenSecretsManager::new(access_token)))
-}
-
-fn get_amqp_connection_config(
-    secrets_manager: &Arc<dyn SecretsManager>,
-) -> Result<AmqpConnectConfig, Error> {
-    let secret_id = match std::env::var(AMQP_CONNECTION_CONFIG_SECRET) {
-        Ok(secret_id) => secret_id,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!(
-                    "failed to read secret id '{}'",
-                    AMQP_CONNECTION_CONFIG_SECRET
-                ),
-            ));
-        }
-    };
-
-    let amqp_connection_config_json = match secrets_manager.get(&secret_id) {
-        Some(amqp_connection_config_json) => amqp_connection_config_json,
-        None => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!("no secret with id '{}'", &secret_id),
-            ));
-        }
-    };
-
-    let amqp_connection_config =
-        match serde_json::from_str::<AmqpConnectConfig>(&amqp_connection_config_json) {
-            Ok(amqp_connection_config) => amqp_connection_config,
-            Err(error) => {
-                return Err(Error::new(
-                    ErrorKind::InvalidData,
-                    format!("secret contains invalid amqp connection config: {}", &error),
-                ));
-            }
-        };
-
-    Ok(amqp_connection_config)
-}
-
-fn get_amqp_api() -> Result<Vec<AmqpApiEntry>, Error> {
-    let amqp_api_file = match std::env::args().nth(0) {
-        Some(amqp_api_file) => amqp_api_file,
-        None => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                "no amqp api file provided",
-            ));
-        }
-    };
-
-    let amqp_api_file_content = match std::fs::read_to_string(&amqp_api_file) {
-        Ok(content) => content,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::NotFound,
-                format!(
-                    "failed to find amqp api file '{}': {}",
-                    &amqp_api_file, &error
-                ),
-            ))
-        }
-    };
-
-    let amqp_api = match serde_json::from_str::<Vec<AmqpApiEntry>>(&amqp_api_file_content) {
-        Ok(amqp_api) => amqp_api,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::InvalidData,
-                format!("failed to deserialize AMQP API file: {}", &error),
-            ))
-        }
-    };
-
-    Ok(amqp_api)
-}
-
-fn get_openid_connect_config() -> Result<OpenIdConnectConfig, Error> {
-    let openid_connect_config_file = match std::env::args().nth(0) {
-        Some(openid_connect_config_file) => openid_connect_config_file,
-        None => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                "no openid connect config file provided",
-            ));
-        }
-    };
-
-    let openid_connect_config_file_content =
-        match std::fs::read_to_string(&openid_connect_config_file) {
-            Ok(content) => content,
-            Err(error) => {
-                return Err(Error::new(
-                    ErrorKind::NotFound,
-                    format!(
-                        "failed to find openid connect config file '{}': {}",
-                        &openid_connect_config_file, &error
-                    ),
-                ))
-            }
-        };
-
-    let openid_connect_config =
-        match serde_json::from_str::<OpenIdConnectConfig>(&openid_connect_config_file_content) {
-            Ok(openid_connect_config) => openid_connect_config,
-            Err(error) => {
-                return Err(Error::new(
-                    ErrorKind::InvalidData,
-                    format!(
-                        "failed to deserialize OpenID Connect config file: {}",
-                        &error
-                    ),
-                ))
-            }
-        };
-
-    Ok(openid_connect_config)
-}
-
-fn get_mongodb_client(secrets_manager: &Arc<dyn SecretsManager>) -> Result<Client, Error> {
-    let secret_id = match std::env::var(MONGODB_CONNECTION_CONFIG_SECRET) {
-        Ok(secret_id) => secret_id,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!(
-                    "failed to read secret id '{}'",
-                    MONGODB_CONNECTION_CONFIG_SECRET
-                ),
-            ));
-        }
-    };
-
-    let mongodb_connection_config_json = match secrets_manager.get(&secret_id) {
-        Some(mongodb_connection_config_json) => mongodb_connection_config_json,
-        None => {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!("no secret with id '{}'", &secret_id),
-            ));
-        }
-    };
-
-    let mongodb_client_options =
-        match serde_json::from_str::<ClientOptions>(&mongodb_connection_config_json) {
-            Ok(mongodb_client_options) => mongodb_client_options,
-            Err(error) => {
-                return Err(Error::new(
-                    ErrorKind::InvalidData,
-                    format!(
-                        "failed to deserialize MongoDB connection config: {}",
-                        &error
-                    ),
-                ))
-            }
-        };
-
-    let mongodb_client = match Client::with_options(mongodb_client_options) {
-        Ok(mongodb_client) => mongodb_client,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::InvalidData,
-                format!("failed to create MongoDB client: {}", &error),
-            ))
-        }
-    };
-
-    Ok(mongodb_client)
 }
