0bddb0d74ab5d363fdb250c8ed8c31e807bae94a
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-17 13:54:32 +0200

    + create '.ssh' directory within runner

--- cd.sh
+++ cd.sh
@@ -2,6 +2,7 @@
 
 CP_ORGANIZATION_SSH_PRIVATE_KEY=$(bws secret get "$CP_ORGANIZATION_SSH_KEY_SECRET" --access-token "$CP_ORGANIZATION_SECRETS_MANAGER_ACCESS_TOKEN" | jq -r '.value')
 
+mkdir ~/.ssh
 echo "$CP_ORGANIZATION_SSH_PRIVATE_KEY" > ~/.ssh/cp_organization_ssh_private_key
 
 eval `ssh-agent`
