31ef5b9302af5ae1e48b82c4a9ea4e5fe46cdf51
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-24 02:36:38 +0200

    + differentiate between dev modes

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,16 @@ if [[ -z "$CP_ORGANIZATION_SECRETS_MANAGER_ACCESS_TOKEN" ]]; then
   exit 1
 fi
 
-export $(cat .env | xargs)
+if [[ $CP_ENVIRONMENT -eq 0 ]]; then
+  echo "Local development mode"
+  export $(cat ./env/local.env | xargs)
+elif [[ $CP_ENVIRONMENT -eq 1 ]]; then
+  echo "Github Actions development mode"
+  export $(cat ./env/actions.env | xargs)
+else
+  echo "Default local development mode"
+  export $(cat ./env/local.env | xargs)
+fi
 
 cargo build
 
@@ -23,8 +32,6 @@ mv ./target/debug/cp-organization ./cp-organization
 mv ./target/debug/test_create_organization_successfully ./test_create_organization_successfully
 
 result_exit_code=0
-
-# TEST CREATE ORGANIZATION SUCCESSFULLY, EXPECTED EXIT CODE: 0
 
 DEFAULT_AMQP_API_FILE="./config/dev/amqp_api.json"
 export CP_ORGANIZATION_AMQP_API_FILE=${CP_ORGANIZATION_AMQP_API_FILE:=$DEFAULT_AMQP_API_FILE}
@@ -44,6 +51,8 @@ db_init() {
 # -----------------------
 
 db_init
+
+# TEST CREATE ORGANIZATION SUCCESSFULLY, EXPECTED EXIT CODE: 0
 
 CP_ORGANIZATION_TEST_AMQP_CONNECTION_URI=$(bws secret get $CP_ORGANIZATION_TEST_AMQP_CONNECTION_URI_SECRET --access-token $CP_ORGANIZATION_SECRETS_MANAGER_ACCESS_TOKEN | jq -r '.value')
 
